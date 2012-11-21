0ecad68832fb844f8aef5b775e630d41441e25ee
Ryuichi KAWAMATA <i@rkmathi.net>
2012-11-21 20:25:07 +0900

    Add ln-config.sh

---
+++ ln-config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# ln -s `pwd`/.gvimrc ~/.gvimrc
+ln -s `pwd`/.gitconfig ~/.gitconfig
+ln -s `pwd`/.gitignore-rkmathi ~/.gitignore-rkmathi
+ln -s `pwd`/.hgrc ~/.hgrc
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/.tmux ~/.tmux
+ln -s `pwd`/.vim ~/.vim
+ln -s `pwd`/.zsh ~/.zsh
